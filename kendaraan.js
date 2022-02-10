ad9c7f0816bb67fbb419cd85baf6cc3b8b6c442a
ayulestari1234 <97926125+ayulestari1234@users.noreply.github.com>
2022-02-10 08:56:17 +0700

    Add files via upload

---
+++ kendaraan.js
@@ -0,0 +1,13 @@
+class Atuthentication {
+          constructor(username,passwoard) {
+
+          }
+}
+class login extends Atuthentication {
+          login() {
+                    console.log();
+          }
+}
+
+let login = new login('ayu','1');
+login.login()
